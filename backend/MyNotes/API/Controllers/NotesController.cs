44e2f06ca414088f48b00ac74b6cc3200ed04389
Nursat <makhsotnursat@gmail.com>
2024-06-14 14:02:49 +0500

    Replace gitignore file in root directory and add README file, then add empty backend project

---
+++ backend/MyNotes/API/Controllers/NotesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NotesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+}
